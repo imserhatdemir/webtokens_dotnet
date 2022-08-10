91affcc1e374dc629a25a4a1137b60da06fa2927
Serhat Demir <64663453+wzarok@users.noreply.github.com>
2022-08-10 16:53:35 +0300

    Add project files.

---
+++ WebApiTut/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiTut
+{
+    public class UserDTO
+    {
+        public string Username { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+    }
+}
